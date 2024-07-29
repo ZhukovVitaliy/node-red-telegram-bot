6644a84c0a4da9bee6e5f7dbbb89ae6f0408fc2e
Zhukov Vitaliy <felex180590@gmail.com>
2024-07-29 18:36:53 +0300

    add Glitch

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
+const http = require("http");
 const express = require("express");
 const RED = require("node-red");
-const path = require("path");
 
+// Создаем сервер
 const app = express();
-const server = require("http").createServer(app);
+const server = http.createServer(app);
 
+// Создаем настройки Node-RED
 const settings = {
   httpAdminRoot: "/admin",
   httpNodeRoot: "/",
-  userDir: path.join(__dirname, "data"),
-  functionGlobalContext: {},
+  userDir: "./.nodered/",
+  flowFile: "flows.json",
+  functionGlobalContext: {}, // Enables global context
 };
 
+// Инициализируем Node-RED
 RED.init(server, settings);
 
+// Указываем Node-RED использовать Express сервер
 app.use(settings.httpAdminRoot, RED.httpAdmin);
 app.use(settings.httpNodeRoot, RED.httpNode);
 
-RED.start()
-  .then(() => {
-    server.listen(process.env.PORT || 3000, () => {
-      console.log("Node-RED server is running");
-    });
-  })
-  .catch((err) => {
-    console.error("Failed to start Node-RED:", err);
-  });
+// Запускаем сервер
+server.listen(process.env.PORT || 3000);
+
+// Запускаем Node-RED
+RED.start();
